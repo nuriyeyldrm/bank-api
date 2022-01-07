b761ed60a8af263b7759fe915cbe319eaac12e67
Nuriye <nry95yldrm@gmail.com>
2022-01-07 16:32:22 +0300

    cors config added

--- src/main/java/com/backend/bankapi/BankApiApplication.java
+++ src/main/java/com/backend/bankapi/BankApiApplication.java
@@ -2,35 +2,12 @@ package com.backend.bankapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BankApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankApiApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        source.registerCorsConfiguration("/**", config);
-        registrationBean.setFilter(new CorsFilter(source));
-        registrationBean.setOrder(0);
-        return registrationBean;
     }
 
 }
